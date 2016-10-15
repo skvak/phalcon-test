20433fe75afcce4dc05b347583db9926721acd69
skvak <a.kvak17@gmail.com>
2016-10-16 00:35:55 +0300

    remove switch and add new methods in IndexController

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,93 +6,30 @@ use Phalcon\Validation\Validator\PresenceOf;
 
 class IndexController extends ControllerBase
 {
-
     public function indexAction()
     {
     }
 
     public function stepAction()
     {
-        $step = null !== $this->request->getPost("step") ?  $this->request->getPost("step") : 1;
+        $step = null !== $this->request->getPost("step") ? $this->request->getPost("step") : 1;
+        $stepMethod = "step".$step;
 
-        switch ($step) {
-            case 1:
-                if ($this->session->get("user")){
-                    $this->session->remove("user");
-                }
-                return $this->view->pick("index/step1");
-            case 2:
-                if ($this->request->isPost() && $this->security->checkToken()) {
-                    $validation = new validationStep1();
-
-                    $messages = $validation->validate($_POST);
-
-                    if (count($messages)) {
-                        foreach ($messages as $message){
-                            $errors[] = $message;
-                        }
-                        $errors = implode(',<br>', $errors);
-                        $this->flashSession->error($errors);
-
-                        return $this->response->redirect("step1");
-                    }
-
-                    $user = new Users();
-                    $user->fullname = $this->request->getPost("fullname");
-                    $user->email = $this->request->getPost("email");
-                    $user->password = $this->security->hash($this->request->getPost("password"));
-
-                    $this->session->set("user", $user);
-
-                    return $this->view->pick("index/step2");
-                }
-
-                return $this->response->redirect("step1");
-
-            case 3:
-                if ($this->request->isPost() && $this->security->checkToken()) {
-                    $validation = new validationStep2();
-
-                    $messages = $validation->validate($_POST);
-
-                    if (count($messages)) {
-                        foreach ($messages as $message){
-                            $errors[] = $message;
-                        }
-                        $errors = implode(',<br>', $errors);
-                        $this->flashSession->error($errors);
-
-                        return $this->response->redirect("step2");
-                    }
-
-                    $user = $this->session->get("user");
-                    $user->city = $this->request->getPost("city");
-                    $user->address = $this->request->getPost("address");
-                    $this->session->set("user", $user);
-
-                    return $this->view->pick("index/step3");
-                }
-
-                return $this->response->redirect("step2");
-        }
+        return $this->$stepMethod();
     }
 
     public function registrationAction()
     {
-        if ($this->request->isPost() && $this->security->checkToken())
-        {
+        if ($this->request->isPost() && $this->security->checkToken()) {
             $interests = $this->request->getPost("interests");
 
-            foreach ($interests as $interest)
-            {
-                if ($interest == null)
-                {
+            foreach ($interests as $interest) {
+                if ($interest == null) {
                     $message = "The interests is required";
                 }
             }
 
-            if (isset($message))
-            {
+            if (isset($message)) {
                 $this->flashSession->error($message);
                 return $this->view->pick("index/step3");
             }
@@ -107,5 +44,83 @@ class IndexController extends ControllerBase
 
         return $this->view->pick("index/finish");
     }
-}
 
+    /**
+     * get first registration step
+     * @return mixed
+     */
+    protected function step1()
+    {
+        if ($this->session->get("user")) {
+            $this->session->remove("user");
+        }
+
+        return $this->view->pick("index/step1");
+    }
+
+    /**
+     * get second registration step
+     * @return mixed
+     */
+    protected function step2()
+    {
+        if ($this->request->isPost() && $this->security->checkToken()) {
+            $validation = new validationStep1();
+
+            $messages = $validation->validate($_POST);
+
+            if (count($messages)) {
+                foreach ($messages as $message) {
+                    $errors[] = $message;
+                }
+                $errors = implode(',<br>', $errors);
+                $this->flashSession->error($errors);
+
+                return $this->response->redirect("step1");
+            }
+
+            $user = new Users();
+            $user->fullname = $this->request->getPost("fullname");
+            $user->email = $this->request->getPost("email");
+            $user->password = $this->security->hash($this->request->getPost("password"));
+
+            $this->session->set("user", $user);
+
+            return $this->view->pick("index/step2");
+        }
+
+        return $this->response->redirect("step1");
+    }
+
+    /**
+     * get third registration step
+     * @return mixed
+     */
+    protected function step3()
+    {
+        if ($this->request->isPost() && $this->security->checkToken()) {
+            $validation = new validationStep2();
+
+            $messages = $validation->validate($_POST);
+
+            if (count($messages)) {
+                foreach ($messages as $message) {
+                    $errors[] = $message;
+                }
+                $errors = implode(',<br>', $errors);
+                $this->flashSession->error($errors);
+
+                return $this->response->redirect("step2");
+            }
+
+            $user = $this->session->get("user");
+            $user->city = $this->request->getPost("city");
+            $user->address = $this->request->getPost("address");
+            $this->session->set("user", $user);
+
+            return $this->view->pick("index/step3");
+        }
+
+        return $this->response->redirect("step2");
+    }
+}
